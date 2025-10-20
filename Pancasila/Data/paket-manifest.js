4c69a5dd9b304f20676968ad16c3a589f3127f63
Samantaslair <anantasamuel96@gmail.com>
2025-10-20 15:02:10 +0700

    Pancasila mk1 case sensitive

--- Pancasila/Data/paket-manifest.js
+++ Pancasila/Data/paket-manifest.js
@@ -3,18 +3,18 @@ const quizPackages = [
         id: "paket1",
         title: "Sejarah Lahirnya Pancasila",
         description: "Kumpulan soal mengenai BPUPKI, PPKI, dan pidato Soekarno.",
-        file: "data/paket1.js"
+        file: "Data/paket1.js"
     },
     {
         id: "paket2",
         title: "Butir-Butir Pancasila",
         description: "Uji pemahamanmu mengenai implementasi nilai-nilai Pancasila.",
-        file: "data/paket2.js"
+        file: "Data/paket2.js"
     },
     {
         id: "paket3",
         title: "Lambang dan Sila",
         description: "Mencocokkan lambang negara dengan bunyi sila Pancasila.",
-        file: "data/paket3.js"
+        file: "Data/paket3.js"
     }
 ];
